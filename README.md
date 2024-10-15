6a33b2a254337c56b59e54cfdac078fa023d0e7f
fishnsotong <wwzyeo@gmail.com>
2024-10-15 11:47:17 +0800

    fresh start for task 4

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rna-ss
 RNA Secondary Structure Predictor
+
+`preprocessing.py` -- manages data download, train/test split, and saves to disk as `.pkl` files
+for reproducibility. You only need to run this once, unless you want a different data split.
+
+`train.py` -- trains the model, lol
---
+++ train.py
@@ -0,0 +1,18 @@
+from utils import load_data, create_dataloader
+
+def main():
+
+    # load the data (train, test)
+    train_df, test_df = load_data()
+
+    # create dataloaders
+    train_dataloader = create_dataloader(train_df)
+    test_dataloader = create_dataloader(test_df, shuffle=False)
+
+    # TODO: implement the training pipeline
+    
+
+    pass
+
+if __name__ == '__main__':
+    main()
